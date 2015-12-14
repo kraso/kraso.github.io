eb6179e35dcd5e641f5a7903c46fc4fe5081e27c
kraso <kraso@users.noreply.github.com>
2015-12-14 18:07:12 +0100

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,22 @@
+<?php
+
+$name=$_POST["name"];
+$firm=$_POST["firm"];
+$value=$_POST["value"];
+
+echo "$name <br> $firm <br> $value";
+
+if(!empty($name) || !empty($firm) || !empty($value)){
+
+$cvsData = $name . "," . $firm . "," . $value  . "\n"; // Add newline
+
+$fp = fopen("_data/vplacila.csv","a"); // $fp is now the file pointer to file $filename
+
+    if($fp)
+    {
+        fwrite($fp,$cvsData); // Write information to the file
+        fclose($fp); // Close the file
+    }
+}
+?>
+
